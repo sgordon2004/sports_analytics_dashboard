cb95b3c7980c3729440d1e033cb9b90d38787cb8
sgordon2004 <syrrgordon@Syrrs-MacBook-Pro-2.local>
2025-02-13 14:50:55 -0500

    Added a predicted winner using net team rating

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sports_analytics_dashboard
+
+"Predicted Winner" uses net team ratings (Offensive TR - Defensive TR) to decide a winner. The team with the higher net rating is predicted to win.
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 # Importing Flask app instance from __init__.py
 from sports_analytics_dashboard import app
 from sports_analytics_dashboard.nba import todays_games
+from sports_analytics_dashboard.predictor import predict_winner
 # Flask returns rendered templates instead of plain text
 from flask import render_template
 # Use route() decorator to define root route
@@ -11,4 +12,6 @@ def home():
 @app.route('/games')
 def games():
     games = todays_games()
+    for game in games:
+        game["predicted_winner"] = predict_winner(game["home_team"], game["away_team"])
     return render_template("games.html", games=games)
